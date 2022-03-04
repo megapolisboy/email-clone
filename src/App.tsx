3cd26c7f5c2d6ffc4079c0b86a31a24a15e8a69d
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-03-04 14:25:51 +0200

    header is done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <h1>Let's build Gmail</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  Apps,
+  ArrowDropDown,
+  Email,
+  Menu,
+  Notifications,
+  Search,
+} from "@material-ui/icons";
+import "../styles/Header.css";
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <Menu />
+        </IconButton>
+        <div className="emblem">
+          <Email />
+          <h1>Gmail</h1>
+        </div>
+      </div>
+      <div className="header__middle">
+        <Search />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDown className="header__inputCaret" />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <Apps />
+        </IconButton>
+        <IconButton>
+          <Notifications />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
